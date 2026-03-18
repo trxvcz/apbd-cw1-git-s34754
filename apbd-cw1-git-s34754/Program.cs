034937884f5d2087abd5b64dddba505b38d3f2a6
pawel <pawelkoc681@gmail.com>
2026-03-18 18:43:12 +0100

    add: wyświetlenie średniej wartości wpisanych liczb przez uzytkownika

--- apbd-cw1-git-s34754/Program.cs
+++ apbd-cw1-git-s34754/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using apbd_cw1_git_s34754;
+
 List<double> list = new List<double>();
 Console.WriteLine("Write numbers: (write exit to exit) and count average");
 while (true)
@@ -20,5 +22,5 @@ while (true)
     }
     
     Console.Clear();
-    ;
+    Console.WriteLine("Average: " +StatisticHelper.CalculateAverage(list));
 }
