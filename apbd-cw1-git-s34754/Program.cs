1298b947bae853c7fc95d53117257f37bb3275b1
pawel <pawelkoc681@gmail.com>
2026-03-18 18:36:45 +0100

    input danych od użytkownika

--- apbd-cw1-git-s34754/Program.cs
+++ apbd-cw1-git-s34754/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+List<double> list = new List<double>();
+Console.WriteLine("Write numbers: (write exit to exit) and count average");
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input == "exit")
+    {
+        break;
+    }
+
+    if (double.TryParse(input, out double result))
+    {
+        list.Add(result);
+    }
+    else
+    {
+        Console.WriteLine("Niepoprawny numer:");
+    }
+    
+    
+}
