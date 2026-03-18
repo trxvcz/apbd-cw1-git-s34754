bd628959a276b7c9ca83642db281418a98cfffeb
pawel <pawelkoc681@gmail.com>
2026-03-18 18:41:37 +0100

    add: feature Calculate Average do StatisticHelper

--- apbd-cw1-git-s34754/StatisticHelper.cs
+++ apbd-cw1-git-s34754/StatisticHelper.cs
@@ -2,5 +2,14 @@ namespace apbd_cw1_git_s34754;
 
 public class StatisticHelper
 {
-    
+    public static double CalculateAverage(List<double> numbers)
+    {
+        double sum = 0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+        return sum / numbers.Count;
+    }
+
 }
